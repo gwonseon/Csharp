f6eb9da3b5da4e2ebe8bac9270d3b7ea3373f16f
Gwonseon <202003789@hufs.ac.kr>
2023-03-24 15:02:16 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+
+            Game game = new Game();
+
+            while (true)
+            {
+                game.Process();
+
+            }
+        }
+    }
+
+    
+}
